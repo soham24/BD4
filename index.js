c767eb9faf395b9dbaae229a1169a98c83b8cfd7
soham24 <mynameisskr@gmail.com>
2024-12-13 17:53:55 +0530

    d

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let db;
 // Initialize the database connection
 (async () => {
   db = await open({
-    filename: './BD4_Assignment1/database.sqlite', // Make sure the file path is correct
+    filename: './BD4_Assignment1/database.sqlite', 
     driver: sqlite3.Database,
   });
 
